47e6f500b134bf9c077f8fc6b7854fc35f551002
Gortizr <00131725@uca.edu.sv>
2025-06-20 21:08:31 -0600

    Se agrego la inicializacion de niveles hasta el 5

--- src/quizz.cpp
+++ src/quizz.cpp
@@ -1,5 +1,31 @@
-#include <iostream>
-using namespace std;
+#include "funciones.h"
+
+void inicializarNiveles(Nivel niveles[]) {
+    // Nivel 1
+    niveles[0] = {
+         1, "Facil",{/*Poner preguntas de nivel 1.*/},10
+    };
+
+     // Nivel 2
+    niveles[1] = {
+        2, "Basico", {/*Poner preguntad de nivel 2.*/}, 20
+    };
+
+    // Nivel 3
+    niveles[2] = {
+        3, "Intermedio", {/*Poner preguntas de nivel 3.*/}, 30
+    };
+
+    // Nivel 4
+    niveles[3] = {
+        4, "Avanzado", {/*Poner preguntas de nivel 4.*/}, 40
+    };
+
+     // Nivel 5
+    niveles[4] = {
+        5, "Desafiante", {/*Poner preguntas de nivel 5.*/}, 50
+    };
+    
 
 
-
+}
