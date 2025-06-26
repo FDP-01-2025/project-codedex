13387ed733f59ca2e609bdaac32a3c86bfbd9df8
Gary9864 <00021925@uca.edu.sv>
2025-06-26 10:09:20 -0600

    Cambios en funcion mostrarPregunta y main.cpp

--- main.cpp
+++ main.cpp
@@ -3,34 +3,7 @@
 
 int main()
 {
-    int = num_niveles = 10;
-    Nivel niveles[num_niveles]; // Arreglo donde se llenaran las preguntas y niveles
-    inicializarNiveles(niveles);
-
-    int nivelActual = cargarJuego(); // En esta variable se carga el progreso el cual es 0 para iniciar
-    cout << "Bienvenido al Quizz-UCA!" << endl;
-    cout << "Te encuentras en el nivel:  " << nivelActual << endl;
-
-    bool continuar = true;
-    while (continuar && nivelActual < num_niveles) // se hace uso del bucle while para avanzar preguntas y niveles
-    {
-        cout << "Nivel" << niveles[nivelActual].numero << " : " << niveles[nivelActual].dificultad < endl;
-
-        // uso del ciclo for para recorrer las preguntas de cada nivel
-        for (const Pregunta &pregunta : niveles[nivelActual].preguntas)
-        {
-            bool correcta = hacerPregunta(pregunta);
-            if (correcta) // Uso del condicional para saber si la respuesta es correcta o no
-            {
-                cout << "¡Haz respondido muy bien!" << endl;
-            }
-            else
-            {
-                cout << "Más suerte en la proxima :( )" << endl;
-            }
-        }
-    }
-
+    /*
     // El nivel actual va aumentand y se guarda el progreso del jugador al responder de forma correcta o incorrecta
     nivelActual++ guardarProgreso(nivelActual);
 
@@ -52,6 +25,7 @@ int main()
     }
 
     // Se termina el quizz
-    cout << "Gracias por visitar Quizz-UCA, hasta la próxima :)";
+    cout << "Gracias por visitar Quizz-UCA, hasta la próxima :)"; */
+
     return 0;
 }
